8b67b8183c6bdb5e4a1f2c22e0a053775f10ccd9
COMP3100 Ryan Poole <ryan.poole@students.mq.edu.au>
2023-04-02 11:34:31 +1000

    While loop test backup

--- MyClient.java
+++ MyClient.java
@@ -10,11 +10,13 @@ try{
 	boolean getsComplete = false; 
 	int topCores = 0;
 	String store[] = new String[10];
+	String jobs[] = new String[10];
 	String[] storeTemp;
 	int numServers = 0;
 	int coreNum = 0;
 	int jobID = 0;
 	int serverID=0;
+	String[] strcheck;
 
 	dout.write(("HELO\n").getBytes());
 	dout.flush(); 
@@ -33,12 +35,9 @@ try{
 	dout.flush();
 	
 	
-	System.out.println("loop start");
-	
 	
 	str=dis.readLine(); 
 	System.out.println(str);
-	System.out.println("Start While");
 	
 	
 	dout.write(("GETS All\n").getBytes());
@@ -74,44 +73,67 @@ try{
 	System.out.println("No. Servers = " + numServers);
 	getsComplete=true;
 	
-	dout.write(("OK\n").getBytes());
-	dout.flush();
+//	dout.write(("OK\n").getBytes());
+//	dout.flush();
 	
 	
-	dout.write(("SCHD " + jobID + " " + store[0] + " " + serverID + "\n").getBytes());
-	dout.flush();
+//	dout.write(("SCHD " + jobID + " " + store[0] + " " + serverID + "\n").getBytes());
+//	dout.flush();
 	
 	
-	dout.write(("OK\n").getBytes());
-	dout.flush();
+//	dout.write(("OK\n").getBytes());
+//	dout.flush();
 	
-	str=dis.readLine(); 
-	System.out.println(str);
-	
+//	str=dis.readLine(); 
+//	System.out.println(str);
+	int i=0;
 	//while(str!="NONE"){
-		
-		jobID++;
-		serverID++;
+	boolean ready=true;
+	
+	dout.write(("OK\n").getBytes());
+	dout.flush();
+	String check = "Hello";
+	boolean none = false;
+	boolean scheduled=false;
+	//while(none==false){
+	while(i<10){
 		if(serverID>numServers) serverID=0;
-	
-		dout.write(("REDY\n").getBytes());
-		dout.flush();
-	
+		
+//		System.out.println("str =" + str);
+//		strcheck = str.split(" ", 10);
+//		check = strcheck[0];
+//		System.out.println(ready);
+
+		if(str.contains("NONE")) none =true;
+		
+		str=dis.readLine(); 
+		System.out.println("str print = " + str);
+		//jobs = str.split(" ",9);
+		//if(jobs[1]==jobID) scheduled = true;
+		//System.out.println("jobs[1] = " + jobs[1]);
+		
 		dout.write(("OK\n").getBytes());
 		dout.flush();
 		
+		dout.write(("REDY\n").getBytes());
+		dout.flush();
+		
+		String a = String.valueOf(jobID);
+		
+		//if(str.contains("JOBN") && str.contains(a)){
+		if(str.contains("JOBN")){
 		System.out.println("SCHD" + jobID + store[0] + store[1] );
 		dout.write(("SCHD " + jobID + " " + store[0] + " " + serverID + "\n").getBytes());
 		dout.flush();
 	
-	
+		scheduled=true;
+		jobID++;
+		serverID++;
+		i++;
+		}
 		dout.write(("OK\n").getBytes());
 		dout.flush();
-	
-		str=dis.readLine(); 
-		System.out.println(str);
-	
- 	//}
+ 	}
 	
 	dout.write(("QUIT\n").getBytes());
 	dout.flush();
