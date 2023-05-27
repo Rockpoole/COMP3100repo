ca994890721878cd45c11673967bff098f3dc7f9
COMP3100 Ryan Poole <ryan.poole@students.mq.edu.au>
2023-05-27 17:40:46 +1000

    Stage 2 RR v1

--- MyClient.java
+++ MyClient.java
@@ -1,13 +1,12 @@
 import java.io.*;  
 import java.net.*;  
-import java.util.arraylist.*;
 public class MyClient {  
 public static void main(String[] args) {  
 try{      
 	// -- Socket Creation -- //
 	Socket mySocket=new Socket("localhost",50000);  
-	BufferedReader dis = new BufferedReader(new InputStreamReader(s.getInputStream()));
-	DataOutputStream dout=new DataOutputStream(s.getOutputStream()); 
+	BufferedReader dis = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+	DataOutputStream dout=new DataOutputStream(mySocket.getOutputStream()); 
 	
 	// -- Variables -- //
 	String store[] = new String[10];
@@ -15,9 +14,8 @@ try{
 	int core = 0;
 	int memory = 0;
 	int disk = 0;
-	int serverID=0; // Tells LRR which server to schedule job to
-	int jobID = 0; // Tells LRR which job to schedule
-	ArrayList available = new ArrayList();
+	int jobID = 0; // Tells RR which job to schedule
+	boolean unavailable = false;
 
 
 	// -- Handshake -- //
@@ -36,19 +34,34 @@ try{
 	// -- Job Scheduler While Loop -- //
 	while(!str.contains("NONE")){  // WHILE LOOP START
 	
+	String jobSpecs[] = new String[10];
+	
+	if(unavailable==true){
+		dout.write(("DEQJ GQ\n").getBytes());
+		dout.flush();
+		unavailable=false;
+	}
+	
 	// -- Find Server Information -- //
 	dout.write(("REDY\n").getBytes());
 	dout.flush();
 	
 	str=dis.readLine(); 
 	System.out.println(str);
+	jobSpecs=str.split(" ", 7);
+		
+	core = Integer.parseInt(String.valueOf(jobSpecs[4]));
+	memory = Integer.parseInt(String.valueOf(jobSpecs[5]));
+	disk = Integer.parseInt(String.valueOf(jobSpecs[6]));
 	
-	dout.write(("GETS Avail " + core + " " + memory + " " + disk "\n").getBytes()); // Get server data
+	dout.write(("GETS Avail " + core + " " + memory + " " + disk + "\n").getBytes()); // Get server data
 	dout.flush();
 	
 	str=dis.readLine(); 
-	System.out.println(str);
+	System.out.println("String split = " + str.split(" ")[1]);
 	
+	// if(str.split(" ")[1].equals("0")) unavailable = true;
+
 	dout.write(("OK\n").getBytes());
 	dout.flush();
 	
@@ -59,21 +72,15 @@ try{
 		str = dis.readLine();
 		System.out.println(str);
 		
-        	storeTemp = str.split(" ", 9);
-		coreNum = Integer.parseInt(String.valueOf(storeTemp[4])); // Find number of cores in this server type
+		storeTemp = str.split(" ", 9);
     	
-    		if(topCores <= coreNum){ // Find Server type with most cores
-    			topCores=coreNum;
+    		if(i==0){ // Save first available server
     			store = storeTemp;
-    			numServers = Integer.parseInt(String.valueOf(store[1])); // Find number of servers of the largest type
 		}
 	}
 
 	dout.write(("OK\n").getBytes());
 	dout.flush();
-	
-	
-		if(serverID>numServers) serverID=0; // Reset to Server 0 when last server has been utilised
 
 		dout.write(("REDY\n").getBytes());
 		dout.flush();
@@ -88,17 +95,21 @@ try{
 		System.out.println("str print 2 = " + str);
 
 
-		if(str.split(" ")[0].equals("JOBN")){ //Schedule a job if server sends one (Don't schedule one if server sends a JCPL)
-			System.out.println("Schedule Job " + jobID + " to server " + store[0] + " " + serverID );
-			dout.write(("SCHD " + jobID + " " + store[0] + " " + serverID + "\n").getBytes());
+		if(str.split(" ")[0].equals("JOBN") && unavailable==false){ //Schedule a job if server sends one (Don't schedule one if server sends a JCPL)
+			System.out.println("Schedule Job " + jobID + " to server " + store[0] + " " + store[1]);
+			dout.write(("SCHD " + jobID + " " + store[0] + " " + store[1] + "\n").getBytes());
 			dout.flush();
 	
 			while(!str.contains("OK")) str=dis.readLine(); // Wait until server acknowledges schedule
 			
 			jobID++; // Increment to next job
-			serverID++; // Increment to next server
 		} //END IF
-		
+	
+		if(unavailable == true){
+			dout.write(("ENGJ GQ\n").getBytes());
+			dout.flush();
+		}
+	
  	} // END WHILE
 	
 		// -- QUIT -- //
@@ -109,4 +120,4 @@ try{
 	mySocket.close();  
 }catch(Exception e){System.out.println(e);}  
 }  
-} 
+}
