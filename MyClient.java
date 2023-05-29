522a6ddd007542066d79d1c93a7fb01c60a7d15a
COMP3100 Ryan Poole <ryan.poole@students.mq.edu.au>
2023-05-29 13:15:38 +1000

    Stage 2 RR Cleaned up

--- MyClient.java
+++ MyClient.java
@@ -9,101 +9,90 @@ try{
 	DataOutputStream dout=new DataOutputStream(mySocket.getOutputStream()); 
 	
 	// -- Variables -- //
+	String input = new String();
 	String store[] = new String[10];
-	String biggest[] = new String[10];
 	String[] storeTemp;
 	int core = 0;
 	int memory = 0;
 	int disk = 0;
 	int jobID = 0; // Tells RR which job to schedule
-	boolean unavailable = false;
-	boolean first = true;
 
 
 	// -- Handshake -- //
 	dout.write(("HELO\n").getBytes());
 	dout.flush(); 
 	 
-	String str=(String)dis.readLine();
-	System.out.println(str);
+	input=dis.readLine();
+	System.out.println(input);
 	  
 	dout.write(("AUTH ryan\n").getBytes());
 	dout.flush();
 	
-	str=dis.readLine(); 
-	System.out.println(str);
+	input=dis.readLine(); 
+	System.out.println(input);
 	
 	// -- Job Scheduler While Loop -- //
-	while(!str.equals("NONE")){  // WHILE LOOP START
+	while(!input.equals("NONE")){  // WHILE LOOP START
 	
 	String jobSpecs[] = new String[10];
 
-	
 	// -- Find Server Information -- //
 	dout.write(("REDY\n").getBytes());
 	dout.flush();
 	
-	str=dis.readLine(); 
-	System.out.println("First Ready = " + str);
-	if(str.equals("NONE")) break;
+	input=dis.readLine(); 
+	System.out.println("First Ready = " + input);
+	if(input.equals("NONE")) break;
 	
-	if(str.split(" ")[0].equals("JOBN")){
+	if(input.split(" ")[0].equals("JOBN")){ // Begin Job Scheduling
 	
-	jobSpecs=str.split(" ", 7);
+	jobSpecs=input.split(" ", 7);
 	core = Integer.parseInt(String.valueOf(jobSpecs[4]));
 	memory = Integer.parseInt(String.valueOf(jobSpecs[5]));
 	disk = Integer.parseInt(String.valueOf(jobSpecs[6]));
 	
 
-	dout.write(("GETS Avail " + core + " " + memory + " " + disk + "\n").getBytes()); // Get server data
+	dout.write(("GETS Avail " + core + " " + memory + " " + disk + "\n").getBytes()); // Get available server data
 	dout.flush();
-	
-			System.out.println("After GETS = " + str);
-	
-	str=dis.readLine(); 
-	System.out.println("String split = " + str);
+		
+	input=dis.readLine(); 
+	System.out.println("String split = " + input);
 
-
-		if(str.split(" ")[1].equals("0")) {
+		if(input.split(" ")[1].equals("0")) { //If no available servers, get first capable server
 			dout.write(("OK\n").getBytes());
 			dout.flush();
-			str = dis.readLine();
+			input = dis.readLine();
 			dout.write(("GETS Capable " + core + " " + memory + " " + disk + "\n").getBytes());
-			str = dis.readLine();	
+			input = dis.readLine();	
 		}
 
 		dout.write(("OK\n").getBytes());
 		dout.flush();	
 			
-		int nRecs = Integer.parseInt(String.valueOf(str.charAt(5))); // Find Number of Records (Servers)
+		int nRecs = Integer.parseInt(String.valueOf(input.charAt(5))); // Find Number of Records (Servers)
 					System.out.println("nRecs = " + nRecs);
 					
 		for (int i=0; i<nRecs; i++){
-			str = dis.readLine();
-			System.out.println(str);
+			input = dis.readLine();
+			System.out.println(input);
 			
-			storeTemp = str.split(" ", 9);
+			storeTemp = input.split(" ", 9);
 	    	
 	    		if(i==0){ // Save first available server
 	    			store = storeTemp;
 			}
-			if(i==(nRecs-1) & first==true) biggest=storeTemp;
 		}
-
-		dout.write(("OK\n").getBytes());
-		dout.flush();	
-		
 
 		dout.write(("OK\n").getBytes());
 		dout.flush();
 
-		str=dis.readLine();
+		input=dis.readLine();
 
 			System.out.println("Schedule Job " + jobID + " to server " + store[0] + " " + store[1]);
 			dout.write(("SCHD " + jobID + " " + store[0] + " " + store[1] + "\n").getBytes());
 			dout.flush();
 	
-			while(!str.contains("OK")) str=dis.readLine(); // Wait until server acknowledges schedule
+			while(!input.contains("OK")) input=dis.readLine(); // Wait until server acknowledges schedule
 			
 			jobID++; // Increment to next job
 		
